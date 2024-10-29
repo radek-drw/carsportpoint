90cc65a9387f157a9c17fdc0683f6e59cddfe99e
Radek Drweski <rdrweski@gmail.com>
2024-10-29 14:12:41 +0000

    update styles

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const NavItem = ({ label, href }) => {
   return (
     <li>
-      <a
-        className="py-6 px-5 block uppercase font-navigation relative"
-        href={href}
-      >
+      <a className="py-6 px-5 block uppercase font-navigation" href={href}>
         {label}
       </a>
     </li>
