b70288e76a9ac37cd4e47e322c44231487e75cb6
Radek Drweski <rdrweski@gmail.com>
2024-10-30 05:57:57 +0000

    import components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Partners from "./components/Partners";
+import Catalogue from "./components/Catalogue";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <Layout>
-      <Navbar />
-    </Layout>
+    <Router>
+      <Layout>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
