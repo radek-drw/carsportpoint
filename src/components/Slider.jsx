a50ecaac20633d4a0f16dd3c7c0c1cbfa65051a8
Radek Drweski <rdrweski@gmail.com>
2024-11-06 16:59:00 +0000

    add styles for text animations

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,7 +5,7 @@ import { Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-fade";
 
-import { useTrail, animated } from "@react-spring/web";
+import { useTrail, animated, easings } from "@react-spring/web";
 
 import slide1 from "../assets/images/slide1.jpg";
 import slide2 from "../assets/images/slide2.jpg";
@@ -13,42 +13,38 @@ import slide2 from "../assets/images/slide2.jpg";
 const Slider = () => {
   const [resetAnimation, setResetAnimation] = useState(false);
 
-  // Funkcja resetująca animację przy każdej zmianie slajdu
+  // Function to reset the text animation on each slide change
   const handleSlideChange = () => {
     setResetAnimation(true);
-    setTimeout(() => setResetAnimation(false), 0); // natychmiastowy reset animacji
+    setTimeout(() => setResetAnimation(false), 0);
   };
 
   const textAnimationsSlide1 = useTrail(2, {
     from: {
       opacity: 0,
-      transform: "translateY(50px) scale(1.2)",
+      transform: "translateX(-150%) scale(1.4)",
       filter: "blur(10px)",
     },
     to: {
       opacity: 1,
-      transform: "translateY(0px) scale(1)",
+      transform: "translateX(0) scale(1)",
       filter: "blur(0px)",
     },
-    config: { duration: 1500 },
+    config: { duration: 2000, easing: easings.easeInOutCirc },
     reset: resetAnimation,
-    trail: 200, // kontrolowane opóźnienie między animacjami
   });
 
   const textAnimationsSlide2 = useTrail(2, {
     from: {
       opacity: 0,
-      transform: "translateY(50px) scale(1.2)",
-      filter: "blur(10px)",
+      transform: "scale(0.4)",
     },
     to: {
       opacity: 1,
-      transform: "translateY(0px) scale(1)",
-      filter: "blur(0px)",
+      transform: "scale(1)",
     },
-    config: { duration: 1500 },
+    config: { duration: 2000, easing: easings.easeInBack },
     reset: resetAnimation,
-    trail: 200, // kontrolowane opóźnienie między animacjami
   });
 
   return (
@@ -56,10 +52,10 @@ const Slider = () => {
       modules={[Autoplay, EffectFade]}
       effect={"fade"}
       autoplay={{ delay: 3000, disableOnInteraction: false }}
-      speed={1000} // Prędkość przejścia fade
+      speed={1000} // Fade transition speed
       loop={true}
       className="mySwiper h-600 w-full font-primary text-7xl"
-      onSlideChange={handleSlideChange} // Ustawienie funkcji resetującej animację
+      onSlideChange={handleSlideChange} // Setting the function to reset the animation
     >
       {[slide1, slide2].map((slide, index) => (
         <SwiperSlide key={index}>
