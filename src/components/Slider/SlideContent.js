8fc91fc059e3c9e48283f28b0f8922d767ac7542
Radek Drweski <rdrweski@gmail.com>
2024-11-07 18:28:16 +0000

    organize into smaller, single-responsibility components

---
+++ src/components/Slider/SlideContent.js
@@ -0,0 +1,17 @@
+import { animated } from "@react-spring/web";
+
+const SlideContent = ({ textAnimations, text, layout }) => (
+  <div className={`text-${layout} capitalize`}>
+    {text.map((line, index) => (
+      <animated.div
+        key={index}
+        style={textAnimations[index]}
+        className={`block ${line.className}`}
+      >
+        {line.content}
+      </animated.div>
+    ))}
+  </div>
+);
+
+export default SlideContent;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 import ProgressBar from "./ProgressBar";
+import SlideContent from "./SlideContent";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-fade";
 
-import { useTrail, animated, easings } from "@react-spring/web";
+import { useSlideAnimations } from "./textAnimations";
 
-import { SLIDER_TIMES, TEXT_ANIMATION } from "./config";
+import { SLIDER_TIMES } from "./config";
 
 import slide1 from "../../assets/images/slide1.jpg";
 import slide2 from "../../assets/images/slide2.jpg";
@@ -20,6 +21,9 @@ const Slider = () => {
   const slideDuration = SLIDER_TIMES.slideDuration;
   const fadeTransitionDuration = SLIDER_TIMES.fadeTransitionDuration;
 
+  const { textAnimationsSlide1, textAnimationsSlide2 } =
+    useSlideAnimations(resetAnimation);
+
   // Function to reset the text animation and progress bar on each slide change
   const handleSlideChange = () => {
     setResetAnimation(true);
@@ -27,39 +31,39 @@ const Slider = () => {
     setTimeout(() => setResetAnimation(false), 0);
   };
 
-  const textAnimationsSlide1 = useTrail(2, {
-    from: {
-      opacity: TEXT_ANIMATION.firstSlide.opacity,
-      transform: `${TEXT_ANIMATION.firstSlide.position} ${TEXT_ANIMATION.firstSlide.zoom}`,
-      filter: TEXT_ANIMATION.firstSlide.blur,
+  const slidesData = [
+    {
+      image: slide1,
+      text: [
+        {
+          content: "a product that can",
+          className: "text-white font-extrabold sm:text-6xl xl:text-7xl",
+        },
+        {
+          content: "change your life",
+          className: "text-white font-extrabold sm:text-6xl xl:text-7xl",
+        },
+      ],
+      animations: textAnimationsSlide1,
+      layout: "left",
     },
-    to: {
-      opacity: 1,
-      transform: "translateX(0) scale(1)",
-      filter: "blur(0px)",
+    {
+      image: slide2,
+      text: [
+        {
+          content: "tuning and remaps",
+          className:
+            "uppercase text-white font-extrabold sm:text-6xl xl:text-7xl",
+        },
+        {
+          content: "We unite the best in Ireland",
+          className: "tracking-4 text-sm text-white",
+        },
+      ],
+      animations: textAnimationsSlide2,
+      layout: "center",
     },
-    config: {
-      duration: TEXT_ANIMATION.firstSlide.animationDuration,
-      easing: easings.easeInOutCirc,
-    },
-    reset: resetAnimation,
-  });
-
-  const textAnimationsSlide2 = useTrail(2, {
-    from: {
-      opacity: TEXT_ANIMATION.secondSlide.opacity,
-      transform: TEXT_ANIMATION.secondSlide.position,
-    },
-    to: {
-      opacity: 1,
-      transform: "scale(1)",
-    },
-    config: {
-      duration: TEXT_ANIMATION.secondSlide.animationDuration,
-      easing: easings.easeInBack,
-    },
-    reset: resetAnimation,
-  });
+  ];
 
   return (
     <div className="relative w-full">
@@ -73,46 +77,20 @@ const Slider = () => {
         className="mySwiper h-600 w-full font-primary text-7xl"
         onSlideChange={handleSlideChange} // Setting the function to reset the animation
       >
-        {[slide1, slide2].map((slide, index) => (
+        {slidesData.map((slide, index) => (
           <SwiperSlide key={index}>
             <div
               className="relative h-full bg-cover bg-center flex items-center justify-center"
               style={{
-                backgroundImage: `url(${slide})`,
+                backgroundImage: `url(${slide.image})`,
                 borderRadius: "15px",
               }}
             >
-              {index === 0 ? (
-                <div className="text-left capitalize">
-                  <animated.div
-                    style={textAnimationsSlide1[0]}
-                    className="block text-white font-extrabold sm:text-6xl xl:text-7xl"
-                  >
-                    a product that can
-                  </animated.div>
-                  <animated.div
-                    style={textAnimationsSlide1[1]}
-                    className="block text-white font-extrabold sm:text-6xl xl:text-7xl"
-                  >
-                    change your life
-                  </animated.div>
-                </div>
-              ) : (
-                <div className="text-center">
-                  <animated.div
-                    style={textAnimationsSlide2[0]}
-                    className="block uppercase text-white font-extrabold sm:text-6xl xl:text-7xl"
-                  >
-                    tuning and remaps
-                  </animated.div>
-                  <animated.div
-                    style={textAnimationsSlide2[1]}
-                    className="block tracking-4 text-sm text-white"
-                  >
-                    We unite the best in Ireland
-                  </animated.div>
-                </div>
-              )}
+              <SlideContent
+                textAnimations={slide.animations}
+                text={slide.text}
+                layout={slide.layout}
+              />
             </div>
           </SwiperSlide>
         ))}
