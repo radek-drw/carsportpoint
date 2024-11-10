41bf9516bb69248a3a1e7557b965e56595f579e5
Radek Drweski <rdrweski@gmail.com>
2024-11-10 10:10:27 +0000

    fix dynamic className for layout

--- src/components/Slider/SlideContent.js
+++ src/components/Slider/SlideContent.js
@@ -1,17 +1,21 @@
 import { animated } from "@react-spring/web";
 
-const SlideContent = ({ textAnimations, text, layout }) => (
-  <div className={`text-${layout}`}>
-    {text.map((line, index) => (
-      <animated.div
-        key={index}
-        style={textAnimations[index]}
-        className={`block ${line.className}`}
-      >
-        {line.content}
-      </animated.div>
-    ))}
-  </div>
-);
+const SlideContent = ({ textAnimations, text, layout }) => {
+  const layoutClass = layout === "left" ? "text-left" : `text-${layout}`;
+
+  return (
+    <div className={layoutClass}>
+      {text.map((line, index) => (
+        <animated.div
+          key={index}
+          style={textAnimations[index]}
+          className={`block ${line.className}`}
+        >
+          {line.content}
+        </animated.div>
+      ))}
+    </div>
+  );
+};
 
 export default SlideContent;
