f867d9453ed506cf8cdb2ef6c69f5b6b44f5a2e6
Radek Drweski <rdrweski@gmail.com>
2024-11-16 00:30:40 +0000

    add reusable Divider component with dynamic orientation and size

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Divider = ({ width }) => {
-  return <div></div>;
+const Divider = ({ size, orientation }) => {
+  const isHorizontal = orientation === "horizontal";
+
+  const styles = isHorizontal
+    ? { width: size, height: "1px" } // Horizontal divider
+    : { height: size, width: "1px" }; // Vertical divider
+
+  return (
+    <div style={styles} className="bg-[#b3b5b7]">
+      <span
+        className={
+          isHorizontal
+            ? "absolute bg-[#20292c] h-1 w-16 -mt-0.5" // Decoration for horizontal
+            : "absolute bg-[#20292c] w-1 h-16 -ml-0.5" // Decoration for vertical
+        }
+      ></span>
+    </div>
+  );
 };
 
 export default Divider;
