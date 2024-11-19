6b800e412ed36d3e8eb18b2e5a1fe1ea8f027d95
Radek Drweski <rdrweski@gmail.com>
2024-11-19 05:58:52 +0000

    apply '@alptugidin/react-circular-progress-bar'

--- src/components/Home/Article/articles/ProgressCircle.jsx
+++ src/components/Home/Article/articles/ProgressCircle.jsx
@@ -1,43 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+import React from "react";
+import { Flat } from "@alptugidin/react-circular-progress-bar";
 
 const CircularProgress = () => {
-  const [progress, setProgress] = useState(0);
-  const targetProgress = 86;
-  const duration = 2;
-
-  useEffect(() => {
-    // Create an IntersectionObserver to trigger progress animation when the element is visible
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setProgress(targetProgress);
-        }
-      },
-      { threshold: 1 } // Trigger when the element is fully visible
-    );
-
-    const element = document.getElementById("circular-progress");
-    if (element) {
-      observer.observe(element);
-    }
-
-    return () => observer.disconnect();
-  }, []);
-
   return (
-    <div id="circular-progress" className="mb-5 w-36 h-36 mx-auto">
-      <CircularProgressbar
-        value={progress}
-        text={`${progress}%`}
-        strokeWidth={3}
-        styles={buildStyles({
-          textColor: "#444",
-          pathColor: "#000",
-          trailColor: "#f9f9f9",
-          pathTransitionDuration: duration,
-        })}
+    <div className="mb-5 w-36 h-36 mx-auto">
+      <Flat
+        progress={86}
+        sx={{
+          strokeColor: "#000",
+          barWidth: 3,
+          bgStrokeColor: "#f9f9f9",
+          bgColor: { value: "#000000", transparency: "00" },
+          valueSize: 24,
+          valueWeight: "normal",
+          valueColor: "#444",
+          loadingTime: 2000,
+          miniCircleColor: "#000",
+          valueFamily: "Raleway",
+          miniCircleSize: 3,
+          intersectionEnabled: true,
+        }}
       />
     </div>
   );
