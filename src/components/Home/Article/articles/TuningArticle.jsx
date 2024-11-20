00bdc8f8e977710f3e9cf20b5af599d36d105079
Radek Drweski <rdrweski@gmail.com>
2024-11-20 17:19:41 +0000

    add styles to left column

--- src/components/Home/Article/articles/TuningArticle.jsx
+++ src/components/Home/Article/articles/TuningArticle.jsx
@@ -10,15 +10,15 @@ import tuning4 from "../../../../assets/images/carsportpoint-tuning-4.jpg";
 const TuningArticle = () => {
   return (
     <article className="flex mx-auto mt-36">
-      <div className="basis-6/12 pl-[20vw]">
+      <div className="basis-6/12 pl-[20vw] pr-20">
         <h2 className="sm:text-3xl lg:text-4xl text-customRed font-bold mb-10">
           Tuning
         </h2>
-        <h3 className="sm:text-5xl xl:text-6xl font-black text-articleHead">
+        <h3 className="sm:text-5xl xl:text-6xl font-black text-articleHead mb-20">
           See how we make our cars perfect
         </h3>
         <Divider size="194px" orientation="horizontal" />
-        <p>
+        <p className="mt-14 lg:text-xl md:text-lg sm:text-base font-medium text-articleHead">
           We only deal with the best so you dont need to worry if your car will
           be in good hands, we make sure any car service on this website is the
           best of the best
