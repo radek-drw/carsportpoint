917dc1fbeea561244b94acea2eae6a51ef2f0687
Radek Drweski <rdrweski@gmail.com>
2024-11-22 22:45:56 +0000

    add background img for 'Tuning' article

--- src/components/Home/Article/articles/ContactArticle.jsx
+++ src/components/Home/Article/articles/ContactArticle.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import home_tuning_sectionbg_1 from "../../../../assets/images/home_tuning_sectionbg_1.png";
+
 const ContactArticle = () => {
-  return <div></div>;
+  return (
+    <article>
+      <div>
+        <img
+          src={home_tuning_sectionbg_1}
+          alt="home_tuning_sectionbg_1"
+          className="absolute"
+        />
+      </div>
+    </article>
+  );
 };
 
 export default ContactArticle;
