25ee4a3bf3265a5c50840948ff5e8792f74f7813
Radek Drweski <rdrweski@gmail.com>
2024-11-28 05:43:34 +0000

    add and style contact article

--- src/components/Home/Article/articles/ContactArticle.jsx
+++ src/components/Home/Article/articles/ContactArticle.jsx
@@ -1,16 +1,43 @@
 import React from "react";
 
-import home_tuning_sectionbg_1 from "../../../../assets/images/home_tuning_sectionbg_1.png";
+import homeContactSectionBg from "../../../../assets/images/home-contact-section-bg.png";
+import about1 from "../../../../assets/images/carsportpoint-contact.jpg";
+
+import Divider from "../../../Divider";
 
 const ContactArticle = () => {
   return (
-    <article>
+    <article className="mt-36">
       <div>
         <img
-          src={home_tuning_sectionbg_1}
-          alt="home_tuning_sectionbg_1"
+          src={homeContactSectionBg}
+          alt="Home contact background"
           className="absolute"
         />
+      </div>
+      <div className="flex">
+        <div className="basis-6/12 pl-[20vw] pr-16">
+          <h2 className="sm:text-3xl lg:text-4xl text-customRed font-bold mb-10">
+            Contact us
+          </h2>
+          <h3 className="sm:text-5xl xl:text-6xl font-black text-articleHead">
+            Want to be a part of something big?
+          </h3>
+          <p className="sm:text-[44px] lg:text-5xl mb-20 mt-4 text-customRed uppercase">
+            get in touch
+          </p>
+          <Divider size="194px" orientation="horizontal" />
+          <p className="mt-14 lg:text-xl md:text-lg sm:text-base font-medium text-articleHead">
+            If you want to be featured on this website submit your request today
+            and our team will be in touch with you as soon as possible
+          </p>
+        </div>
+        <div className="basis-6/12">
+          <img src={about1} alt="about1" />
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Divider size="194px" orientation="vertical" rotate={180} />
       </div>
     </article>
   );
