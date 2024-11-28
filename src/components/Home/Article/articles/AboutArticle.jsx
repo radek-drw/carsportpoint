6ec756c18526bda05c0a60d1b3fcbe14da739c60
Radek Drweski <rdrweski@gmail.com>
2024-11-28 17:00:16 +0000

    add padding to head of 'About' article

--- src/components/Home/Article/articles/AboutArticle.jsx
+++ src/components/Home/Article/articles/AboutArticle.jsx
@@ -17,8 +17,8 @@ const AboutArticle = () => {
         />
       </div>
       <article className="2xl:px-[20vw] xl:px-[14vw] lg:px-[9vw] md:px-[5vw] sm:px-[3vw] px-[2vw]">
-        <div className="flex pb-32">
-          <div className="basis-[66%] pt-40">
+        <div className="flex mb-32">
+          <div className="basis-[66%] pt-40 pr-6">
             <h3 className="text-customRed sm:text-3xl md:text-4xl font-bold mb-9">
               About time you have found us.
             </h3>
