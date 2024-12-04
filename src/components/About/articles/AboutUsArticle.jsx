66b56f8ff82a64da3612b7e9446f8148d9103021
Radek Drweski <rdrweski@gmail.com>
2024-12-04 17:45:32 +0000

    add another part of 'About' article

--- src/components/About/articles/AboutUsArticle.jsx
+++ src/components/About/articles/AboutUsArticle.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import Divider from "../../Divider";
 
 import aboutMain1 from "../../../assets/images/aboutmain1.jpg";
+import sectionBg1 from "../../../assets/images/section_bg1.png";
+import homeTuningAbout1 from "../../../assets/images/home_tuning_about1.png";
 
 const AboutUs = () => {
   return (
     <article>
       <div className="flex">
         <div className="basis-6/12 flex justify-center self-start">
-          <img src={aboutMain1} alt="CarSportPoint2" />
+          <img src={aboutMain1} alt="Modified cars in garage" />
         </div>
         <div className="pt-[70px] px-[4vw] basis-6/12">
           <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-9 text-customRed">
@@ -26,6 +28,18 @@ const AboutUs = () => {
             quis consectetur justo. Sed et interdum turpis? Ut gravida ut purus
             vel luctus? Duis volutpat.
           </h4>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src={sectionBg1} alt="Logo A" className="absolute" />
+        </div>
+        <div>
+          <img
+            src={homeTuningAbout1}
+            alt="Wheel"
+            className="absolute right-0"
+          />
         </div>
       </div>
     </article>
