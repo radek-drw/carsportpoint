3b937e8e12fc9bb08e44d7ad919d923c1bd0f000
Radek Drweski <rdrweski@gmail.com>
2024-12-05 17:50:12 +0000

    add context and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,24 @@ import { LuDot } from "react-icons/lu";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
-          <div>
-            <FaFacebook size={35} />
-            <LuDot />
-            <FaYoutube size={35} />
-            <LuDot />
-            <MdEmail size={35} />
+    <footer className="px-10 pb-20 pt-16">
+      <div className="flex">
+        <div className="basis-[65vw]">
+          <div className="flex items-center text-[#1E1E1E]">
+            <FaFacebook size={38} />
+            <LuDot size={46} className="mx-3 text-footerCustomGrey" />
+            <FaYoutube size={38} />
+            <LuDot size={46} className="mx-3 text-footerCustomGrey" />
+            <MdEmail size={38} />
           </div>
+          <div className="w-[88px] h-[1px] my-9 bg-footerCustomGrey"></div>
+          <p className="text-sm w-2/4 text-footerCustomGrey">
+            Nulla facilisi. Proin ultrices dui in diam euismod, sed iaculis nunc
+            interdum suspendisse vehicula porta. Ut felis erat, faucibus ac
+            ipsum in
+          </p>
         </div>
-        <div>
+        <div className="flex-1">
           <Logo />
         </div>
       </div>
