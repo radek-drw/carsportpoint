ed3d9f588061bc381d1ab6628dd1175bf321b112
Radek Drweski <rdrweski@gmail.com>
2024-12-05 06:03:59 +0000

    add main layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 
+import Logo from "../components/Logo";
+
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { LuDot } from "react-icons/lu";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div>
+        <div>
+          <div>
+            <FaFacebook size={35} />
+            <LuDot />
+            <FaYoutube size={35} />
+            <LuDot />
+            <MdEmail size={35} />
+          </div>
+        </div>
+        <div>
+          <Logo />
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
