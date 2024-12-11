29deec04d7a11cb09de4664c097470cf80a31f5b
Radek Drweski <rdrweski@gmail.com>
2024-12-11 17:39:51 +0000

    improve component with default props

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Divider = ({ size, orientation, rotate }) => {
+const Divider = ({ size = "1px", orientation = "horizontal", rotate = 0 }) => {
   const isHorizontal = orientation === "horizontal";
 
   const styles = {
     ...(isHorizontal
       ? { width: size, height: "1px" } // Horizontal divider
       : { height: size, width: "1px" }), // Vertical divider
-    transform: rotate ? `rotate(${rotate}deg)` : "none",
+    transform: `rotate(${rotate}deg)`,
   };
 
   return (
-    <div style={styles} className="bg-[#b3b5b7]">
+    <div style={styles} className="bg-[#b3b5b7] relative">
       <span
         className={
           isHorizontal
