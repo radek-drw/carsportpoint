c9dfd991ae55738343ae7c5f0f718dbf1adb3fac
Radek Drweski <rdrweski@gmail.com>
2024-12-15 13:48:21 +0000

    add 'AddCompanyForm' component

--- src/components/CataloguePage/CataloguePage.jsx
+++ src/components/CataloguePage/CataloguePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import CatalogueHeader from "./CatalogueHeader";
+import AddCompanyForm from "./AddCompanyForm";
 
 const CataloguePage = () => {
   return (
     <section className="pt-16">
       <CatalogueHeader />
+      <AddCompanyForm />
     </section>
   );
 };
