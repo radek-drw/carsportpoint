5889db3849bdb76785e36a8c37af117d8c488ca9
Radek Drweski <rdrweski@gmail.com>
2024-12-27 17:22:11 +0000

    format code with Prettier and sort Tailwind CSS classes

--- src/components/CataloguePage/ContactForm.jsx
+++ src/components/CataloguePage/ContactForm.jsx
@@ -23,7 +23,7 @@ const validationSchema = Yup.object({
   email: Yup.string()
     .matches(
       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Invalid email address"
+      "Invalid email address",
     )
     .required("Email is required"),
   phone: Yup.string().test(
@@ -33,10 +33,10 @@ const validationSchema = Yup.object({
       if (!value) return true;
       const phoneNumber = parsePhoneNumberFromString(
         value,
-        context.parent.country
+        context.parent.country,
       );
       return phoneNumber && phoneNumber.isValid();
-    }
+    },
   ),
 
   description: Yup.string().trim().required("Message is required"),
@@ -49,12 +49,12 @@ const validationSchema = Yup.object({
         (1024 * 1024)
       ).toFixed()}MB.`,
       (files) =>
-        files ? files.every((file) => file.size <= MAX_FILE_SIZE) : true
+        files ? files.every((file) => file.size <= MAX_FILE_SIZE) : true,
     )
     .test("fileType", "Invalid file type.", (files) =>
       files
         ? files.every((file) => SUPPORTED_FORMATS.includes(file.type))
-        : true
+        : true,
     ),
 });
 
@@ -82,7 +82,7 @@ const AddCompanyForm = () => {
           const selectedFiles = Array.from(event.currentTarget.files);
           const newFiles = [...values.files, ...selectedFiles].slice(
             0,
-            MAX_FILES
+            MAX_FILES,
           );
           setFieldValue("files", newFiles);
 
@@ -97,16 +97,16 @@ const AddCompanyForm = () => {
 
         return (
           <Form
-            className="mx-auto xl:max-w-[732px] lg:max-w-[630px] md:max-w-[524px] mt-20"
+            className="mx-auto mt-20 md:max-w-[524px] lg:max-w-[630px] xl:max-w-[732px]"
             noValidate
           >
-            <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-8 text-customRed">
+            <h3 className="mb-8 text-2xl font-bold text-customRed sm:text-3xl md:text-4xl">
               Do you want to add your company? <br /> Please fill the form
             </h3>
 
             {/* Company name */}
             <div className="mb-8">
-              <label htmlFor="text" className="block text-sm font-medium mb-1">
+              <label htmlFor="text" className="mb-1 block text-sm font-medium">
                 Company Name
               </label>
               <Field
@@ -118,16 +118,16 @@ const AddCompanyForm = () => {
               <ErrorMessage
                 name="companyName"
                 component="div"
-                className="text-red-500 text-sm mt-1"
+                className="mt-1 text-sm text-red-500"
               />
             </div>
 
             <div className="flex justify-between">
               {/* E-mail */}
-              <div className="basis-[47%] mb-8">
+              <div className="mb-8 basis-[47%]">
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium mb-1"
+                  className="mb-1 block text-sm font-medium"
                 >
                   Email Address *
                 </label>
@@ -143,16 +143,16 @@ const AddCompanyForm = () => {
                 <ErrorMessage
                   name="email"
                   component="div"
-                  className="text-red-500 text-sm mt-1 font-bold"
+                  className="mt-1 text-sm font-bold text-red-500"
                   id="email-error"
                 />
               </div>
 
               {/* Phone */}
-              <div className="basis-[47%] mb-8">
+              <div className="mb-8 basis-[47%]">
                 <label
                   htmlFor="phone"
-                  className="block text-sm font-medium mb-1"
+                  className="mb-1 block text-sm font-medium"
                 >
                   Phone Number
                 </label>
@@ -167,7 +167,7 @@ const AddCompanyForm = () => {
                 <ErrorMessage
                   name="phone"
                   component="div"
-                  className="text-red-500 text-sm mt-1 font-bold"
+                  className="mt-1 text-sm font-bold text-red-500"
                 />
               </div>
             </div>
@@ -176,7 +176,7 @@ const AddCompanyForm = () => {
             <div className="mb-8">
               <label
                 htmlFor="description"
-                className="block text-sm font-medium mb-1"
+                className="mb-1 block text-sm font-medium"
               >
                 Company Information *
               </label>
@@ -197,17 +197,17 @@ const AddCompanyForm = () => {
               <ErrorMessage
                 name="description"
                 component="div"
-                className="text-red-500 text-sm mt-0.5 font-bold"
+                className="mt-0.5 text-sm font-bold text-red-500"
               />
             </div>
 
             {/* Files */}
             <div className="mb-8">
               <label
-                className={`block w-full px-4 py-2 text-center text-white rounded ${
+                className={`block w-full rounded px-4 py-2 text-center text-white ${
                   values.files.length >= MAX_FILES
-                    ? "bg-gray-300 cursor-not-allowed"
-                    : "bg-blue-500 hover:bg-blue-700 cursor-pointer duration-300"
+                    ? "cursor-not-allowed bg-gray-300"
+                    : "cursor-pointer bg-blue-500 duration-300 hover:bg-blue-700"
                 }`}
               >
                 {values.files.length >= MAX_FILES
@@ -231,7 +231,7 @@ const AddCompanyForm = () => {
               <ErrorMessage
                 name="files"
                 component="div"
-                className="text-red-500 text-sm mt-1 font-bold"
+                className="mt-1 text-sm font-bold text-red-500"
               />
 
               {/* Display File List */}
@@ -239,18 +239,18 @@ const AddCompanyForm = () => {
                 <ul className="mt-4">
                   {values.files.map((file, index) => {
                     const isInvalidFileType = !SUPPORTED_FORMATS.includes(
-                      file.type
+                      file.type,
                     );
                     const isInvalidFileSize = file.size > MAX_FILE_SIZE;
 
                     return (
                       <li
                         key={index}
-                        className="flex justify-between items-center mb-2 bg-gray-100 p-2 rounded-md"
+                        className="mb-2 flex items-center justify-between rounded-md bg-gray-100 p-2"
                       >
-                        <div className="flex flex-col w-3/4">
+                        <div className="flex w-3/4 flex-col">
                           <span
-                            className={`text-sm truncate ${
+                            className={`truncate text-sm ${
                               isInvalidFileType || isInvalidFileSize
                                 ? "text-gray-500 line-through"
                                 : "text-green-500"
@@ -261,14 +261,14 @@ const AddCompanyForm = () => {
                           </span>
                           {/* Display error messages for invalid file type */}
                           {isInvalidFileType && (
-                            <span className="text-red-500 text-xs flex items-center">
+                            <span className="flex items-center text-xs text-red-500">
                               Unsupported file type.
-                              <span className="relative group ml-2">
+                              <span className="group relative ml-2">
                                 <FaInfoCircle
-                                  className="text-blue-500 cursor-pointer group-hover:text-blue-700 text-base"
+                                  className="cursor-pointer text-base text-blue-500 group-hover:text-blue-700"
                                   aria-label="Supported file types"
                                 />
-                                <span className="absolute left-0 mt-2 z-10   bg-gray-800 text-white text-xs rounded-lg shadow-lg p-3   transition-opacity duration-500 ease-in-out opacity-0 group-hover:opacity-100 invisible group-hover:visible   whitespace-nowrap">
+                                <span className="invisible absolute left-0 z-10 mt-2 whitespace-nowrap rounded-lg bg-gray-800 p-3 text-xs text-white opacity-0 shadow-lg transition-opacity duration-500 ease-in-out group-hover:visible group-hover:opacity-100">
                                   <strong>Supported formats: </strong>
                                   JPG, JPEG, PNG, GIF, PDF, DOC, DOCX, TXT
                                 </span>
@@ -278,8 +278,8 @@ const AddCompanyForm = () => {
 
                           {/* Display error messages for invalid file sizes */}
                           {isInvalidFileSize && (
-                            <span className="text-red-500 text-xs">
-                              <span className="text-red-500 text-xs">
+                            <span className="text-xs text-red-500">
+                              <span className="text-xs text-red-500">
                                 File size exceeds{" "}
                                 {(MAX_FILE_SIZE / (1024 * 1024)).toFixed()}MB.
                                 Please upload a smaller file.
@@ -290,7 +290,7 @@ const AddCompanyForm = () => {
                         <button
                           type="button"
                           onClick={() => handleFileRemove(index)}
-                          className="text-red-500 hover:text-red-700 font-bold text-sm duration-300"
+                          className="text-sm font-bold text-red-500 duration-300 hover:text-red-700"
                         >
                           Remove
                         </button>
@@ -305,7 +305,7 @@ const AddCompanyForm = () => {
             <button
               type="submit"
               disabled={isSubmitting}
-              className="block text-white bg-red-500 w-full px-5 py-4 rounded-md hover:bg-red-700 duration-300"
+              className="block w-full rounded-md bg-red-500 px-5 py-4 text-white duration-300 hover:bg-red-700"
             >
               SEND NOW
             </button>
