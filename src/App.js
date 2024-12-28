2d0fc0b80136ed5b825c7786797e7a7ff54dc57a
Radek Drweski <rdrweski@gmail.com>
2024-12-28 18:10:10 +0000

    update import paths and streamline layout structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Layout from "./components/Layout";
-import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./components/HomePage/HomePage";
-import AboutUsPage from "./components/AboutUsPage/AboutUsPage";
-import PartnersPage from "./components/PartnersPage/PartnersPage";
-import CataloguePage from "./components/CataloguePage/CataloguePage";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Layout from "./components/layout/Layout";
+import HomePage from "./components/pages/HomePage/HomePage";
+import AboutUsPage from "./components/pages/AboutUsPage/AboutUsPage";
+import PartnersPage from "./components/pages/PartnersPage/PartnersPage";
+import CataloguePage from "./components/pages/CataloguePage/CataloguePage";
+import Contact from "./components/pages/ContactPage/ContactPage";
 
 function App() {
   return (
     <Router>
       <Layout>
-        <Navbar />
-        <main className="font-main">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutUsPage />} />
-            <Route path="/partners" element={<PartnersPage />} />
-            <Route path="/catalogue" element={<CataloguePage />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/catalogue" element={<CataloguePage />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </Layout>
     </Router>
   );
