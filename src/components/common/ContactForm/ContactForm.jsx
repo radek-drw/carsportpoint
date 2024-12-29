e2365e85a3192043868ff5c6876ca9cde6a09322
Radek Drweski <rdrweski@gmail.com>
2024-12-29 15:37:03 +0000

    update component to accept dynamic props

--- src/components/common/ContactForm/ContactForm.jsx
+++ src/components/common/ContactForm/ContactForm.jsx
@@ -5,7 +5,18 @@ import MessageSection from "./MessageSection";
 import FileUploadSection from "./FileUploadSection";
 import { validationSchema } from "./validationSchema";
 
-const ContactForm = () => {
+const ContactForm = ({
+  title,
+  titleStyle,
+  subtitle,
+  subtitleStyle,
+  nameLabel,
+  messageLabel,
+  messagePlaceholder,
+  messageFieldRows,
+  includeFileInput = false,
+  submitButtonTxt,
+}) => {
   const [country, setCountry] = useState("IE");
 
   return (
@@ -29,23 +40,29 @@ const ContactForm = () => {
           className="mx-auto mt-20 md:max-w-[524px] lg:max-w-[630px] xl:max-w-[732px]"
           noValidate
         >
-          <h3 className="mb-8 text-2xl font-bold text-customRed sm:text-3xl md:text-4xl">
-            Do you want to add your company? <br /> Please fill the form
-          </h3>
+          {title && <h3 className={`${titleStyle}`}>{title}</h3>}
+          {subtitle && <h4 className={`${subtitleStyle}`}>{subtitle}</h4>}
           <CompanyInfoSection
             values={values}
             setFieldValue={setFieldValue}
             country={country}
             setCountry={setCountry}
+            nameLabel={nameLabel}
           />
-          <MessageSection />
-          <FileUploadSection values={values} setFieldValue={setFieldValue} />
+          <MessageSection
+            messageFieldRows={messageFieldRows}
+            messageLabel={messageLabel}
+            messagePlaceholder={messagePlaceholder}
+          />
+          {includeFileInput && (
+            <FileUploadSection values={values} setFieldValue={setFieldValue} />
+          )}
           <button
             type="submit"
             disabled={isSubmitting}
-            className="duration-default block w-full rounded-md bg-red-500 px-5 py-4 text-white hover:bg-red-700"
+            className="block w-full rounded-md bg-red-500 px-5 py-4 text-white duration-default hover:bg-red-700"
           >
-            SEND NOW
+            {submitButtonTxt}
           </button>
         </Form>
       )}
