b7983904a25f8e8beff8a1611a959f2678d5c277
Radek Drweski <rdrweski@gmail.com>
2024-12-29 15:36:42 +0000

    update component with dynamic props

--- src/components/pages/CataloguePage/CataloguePage.jsx
+++ src/components/pages/CataloguePage/CataloguePage.jsx
@@ -7,7 +7,25 @@ const CataloguePage = () => {
   return (
     <section className="pt-16">
       <CatalogueHeader />
-      <ContactForm />
+      <ContactForm
+        title={
+          <>
+            Do you want to add your company?
+            <span className="mt-2 block">Please fill the form</span>
+          </>
+        }
+        titleStyle={
+          "mb-8 text-2xl font-bold text-customRed sm:text-3xl md:text-4xl"
+        }
+        messageFieldRows={9}
+        includeFileInput={true}
+        submitButtonTxt={"SEND NOW"}
+        nameLabel={"Company name"}
+        messageLabel={"Company Information *"}
+        messagePlaceholder={
+          "Please provide some information about your company: opening hours, address, contact details, and any other important info"
+        }
+      />
     </section>
   );
 };
