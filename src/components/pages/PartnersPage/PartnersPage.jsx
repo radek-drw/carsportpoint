318f201eadf32f9a47eef3e5c6a57962df7c9435
Radek Drweski <rdrweski@gmail.com>
2025-01-08 16:56:57 +0000

    refactor component for improved accessibility and semantic structure

--- src/components/pages/PartnersPage/PartnersPage.jsx
+++ src/components/pages/PartnersPage/PartnersPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Divider from "../../common/Divider";
 
 import engineImg from "../../../../src/assets/images/home_tuning_offer1.jpg";
+
 import aCunningham from "../../../../src/assets/images/A-Cunningham-1024x768-1.png";
 import abWight from "../../../../src/assets/images/AB-wight-1024x768-1.png";
 import elliotHenderson from "../../../../src/assets/images/Elliot-Henderson-1024x768-1.png";
@@ -15,31 +16,38 @@ import starvin from "../../../../src/assets/images/starvin-1024x768-1.png";
 import monteCassino from "../../../../src/assets/images/monte-cassino-1024x768-1.png";
 
 const PartnersPage = () => {
-  const images = [
-    aCunningham,
-    abWight,
-    elliotHenderson,
-    galaHarriers,
-    marshallTractors,
-    motoCarSales,
-    peterHogg,
-    spg,
-    starvin,
-    monteCassino,
+  const partners = [
+    { src: aCunningham, name: "A. Cunningham" },
+    { src: abWight, name: "AB Wight" },
+    { src: elliotHenderson, name: "Elliot Henderson" },
+    { src: galaHarriers, name: "Gala Harriers" },
+    { src: marshallTractors, name: "Marshall Tractors" },
+    { src: motoCarSales, name: "Moto Car Sales" },
+    { src: peterHogg, name: "Peter Hogg" },
+    { src: spg, name: "SPG Fire and Security" },
+    { src: starvin, name: "Starvin" },
+    { src: monteCassino, name: "Monte Cassino" },
   ];
   return (
-    <section>
+    <section aria-labelledby="our-partners-section">
       <article>
         <div className="flex pl-[4vw]">
           <div className="basis-1/2 pr-[3vw] pt-36">
-            <h3 className="mb-9 text-2xl font-bold uppercase text-customRed sm:text-3xl md:text-4xl">
+            <h1
+              className="mb-9 text-2xl font-bold uppercase text-customRed sm:text-3xl md:text-4xl"
+              id="our-partners-section"
+              aria-describedby="our-partners-desc"
+            >
               our partners
-            </h3>
+            </h1>
             <h2 className="mb-20 text-4xl font-black tracking-wide text-articleHead sm:text-5xl lg:text-6xl">
               We cooperate with the best in the industry
             </h2>
             <Divider size="194px" orientation="horizontal" />
-            <h4 className="mb-4 mt-14 pr-[46%] text-sm font-medium text-articleHead sm:text-base md:text-lg lg:text-xl">
+            <h4
+              className="mb-4 mt-14 pr-[46%] text-sm font-medium text-articleHead sm:text-base md:text-lg lg:text-xl"
+              id="our-partners-desc"
+            >
               Mauris vel tellus in urna volutpat tempus? Proin viverra varius
               massa nec commodo. Cras aliquet leo ut tellus.
             </h4>
@@ -50,15 +58,15 @@ const PartnersPage = () => {
             </p>
           </div>
           <Divider size="128px" orientation="vertical" />
-          <div className="basis-1/2 pl-[2vw] pt-12">
+          <figure className="basis-1/2 pl-[2vw] pt-12">
             <img src={engineImg} alt="Car engine" className="ml-auto" />
-          </div>
+          </figure>
         </div>
         <div className="mx-auto grid grid-cols-5 gap-4 pb-10 pt-5 md:max-w-[748px] lg:max-w-[1004px] xl:max-w-[1260px]">
-          {images.map((src, index) => (
-            <div key={index} className="flex items-center justify-center">
-              <img src={src} alt="Our partners" />
-            </div>
+          {partners.map((partner, index) => (
+            <figure key={index} className="flex items-center justify-center">
+              <img src={partner.src} alt={`Logo of ${partner.name}`} />
+            </figure>
           ))}
         </div>
       </article>
