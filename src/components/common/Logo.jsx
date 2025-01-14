c3ead3d571df62bdb4767a9194349b9c45fcf780
Radek Drweski <rdrweski@gmail.com>
2025-01-14 04:52:04 +0000

    enhance component for accessibility and interactivity

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/carsportpoint_logo.png";
 
-const Logo = ({ isClickable = false, className = "" }) => {
-  const logoImage = <img src={logo} alt="Car Sport Point" />;
+const Logo = ({ isClickable = false, className = "", onClick }) => {
+  const logoImage = (
+    <img src={logo} alt={isClickable ? "Car Sport Point" : ""} />
+  );
 
   return (
     <div className={className}>
-      {isClickable ? <Link to="/">{logoImage}</Link> : logoImage}
+      {isClickable ? (
+        <Link to="/" onClick={onClick} aria-label="Go to homepage">
+          {logoImage}
+        </Link>
+      ) : (
+        <div aria-hidden="true">{logoImage}</div>
+      )}
     </div>
   );
 };
