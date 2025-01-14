5d5eead5cd8e4e4b6699b4f1f4d3f93e372c729f
Radek Drweski <rdrweski@gmail.com>
2025-01-14 04:53:08 +0000

    refactor  component for improved class management

--- src/components/layout/Navbar/NavItem.jsx
+++ src/components/layout/Navbar/NavItem.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavItem = ({ label, href, isActive, onClick }) => {
+  const baseClasses =
+    "block px-4 py-10 font-navigation uppercase text-navDesktop";
+  const activeClasses = "border-b-4 border-customRed text-customRed";
+  const hoverClasses =
+    "hover:border-b-4 hover:border-customRed hover:text-customRed";
+
   return (
     <li onClick={onClick} className="mr-4">
       <Link
         to={href}
-        className={`block px-4 py-10 font-navigation uppercase text-navDesktop ${
-          isActive
-            ? "border-b-4 border-customRed text-customRed"
-            : "hover:border-b-4 hover:border-customRed hover:text-customRed"
-        }`}
+        className={`${baseClasses} ${isActive ? activeClasses : hoverClasses}`}
         aria-current={isActive ? "page" : undefined}
       >
         {label}
