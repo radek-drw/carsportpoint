15184d95039877b9378f069387e5301d8b670b12
Radek Drweski <rdrweski@gmail.com>
2025-01-21 05:59:08 +0000

    add ARIA attributes for better accessibility

--- src/components/common/ContactForm/FileUploadField.jsx
+++ src/components/common/ContactForm/FileUploadField.jsx
@@ -12,13 +12,18 @@ const FileUploadField = ({
   maxFileSize,
   supportedFormats,
 }) => {
+  const errorId = `${name}-error`;
+  const descriptionId = `${name}-description`;
+
   return (
     <div className="mb-input-gap">
       <label
+        htmlFor={name}
         className={`block w-full rounded px-4 py-2 text-center text-white ${files.length >= maxFiles ? "cursor-not-allowed bg-gray-300" : "cursor-pointer bg-blue-500 duration-default hover:bg-blue-700"}`}
       >
         {files.length >= maxFiles ? "File Limit Reached" : label}
         <input
+          id={name}
           type="file"
           name={name}
           multiple
@@ -26,15 +31,19 @@ const FileUploadField = ({
           onChange={onChange}
           className="hidden"
           accept=".jpg,.jpeg,.png,.gif,.pdf,.doc,.docx,.txt"
+          aria-labelledby={name}
+          aria-describedby={`${descriptionId} ${errorId}`}
+          aria-disabled={files.length >= maxFiles}
         />
       </label>
-      <span className="text-sm text-gray-500">
+      <span className="text-sm text-gray-500" id={descriptionId}>
         You can upload up to {maxFiles} files. Each file must be less than{" "}
         {(maxFileSize / (1024 * 1024)).toFixed()}MB
       </span>
       <ErrorMessage
         name={name}
         component="div"
+        id={errorId}
         className="mt-1 text-sm text-red-500"
       />
       {files.length > 0 && (
@@ -59,7 +68,9 @@ const FileUploadField = ({
                       <span className="group relative ml-2">
                         <FaInfoCircle
                           className="cursor-pointer text-base text-blue-500 duration-default group-hover:text-blue-700"
-                          aria-label="Supported file types"
+                          aria-label={
+                            "Supported formats are: jpg, jpeg, png, gif, pdf, doc, docx, txt"
+                          }
                         />
                         <span
                           role="tooltip"
