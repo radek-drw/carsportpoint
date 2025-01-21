3e89c5a93dc18f5198c86250e7b005bba7d8196b
Radek Drweski <rdrweski@gmail.com>
2025-01-21 06:00:11 +0000

    add ARIA attributes for better accessibility

--- src/components/common/ContactForm/SubjectField.jsx
+++ src/components/common/ContactForm/SubjectField.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Field, ErrorMessage } from "formik";
 
-const SubjectField = ({ label, name }) => {
+const SubjectField = ({ label, name, errors, touched }) => {
+  const errorId = `${name}-error`;
+
   return (
     <div className="mb-input-gap">
-      <label htmlFor={name} className="mb-1 block text-sm font-medium">
+      <label
+        htmlFor={name}
+        className="mb-1 block text-sm font-medium"
+        aria-invalid={!!errors[name] && touched[name]}
+        aria-describedby={!!errors[name] && touched[name] ? errorId : undefined}
+      >
         {label}
       </label>
       <Field id={name} name={name} className="input-bordered w-full" />
       <ErrorMessage
         name={name}
         component="div"
+        id={errorId}
         className="mt-1 text-sm text-red-500"
       />
     </div>
