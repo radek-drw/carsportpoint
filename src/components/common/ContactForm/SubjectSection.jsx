2b4b1553f369d9a5d4aad37e94fb84f6c256e6a1
Radek Drweski <rdrweski@gmail.com>
2025-01-21 06:01:16 +0000

    pass error handling props to SubjectField

--- src/components/common/ContactForm/SubjectSection.jsx
+++ src/components/common/ContactForm/SubjectSection.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import SubjectField from "./SubjectField";
 
-const SubjectSection = () => {
-  return <SubjectField label="Subject" name="subject" />;
+const SubjectSection = (errors, touched) => {
+  return (
+    <SubjectField
+      label="Subject"
+      name="subject"
+      errors={errors}
+      touched={touched}
+    />
+  );
 };
 
 export default SubjectSection;
