a9519d6ed2fb3cb71e954589c8ba6a6b6db81cec
Radek Drweski <rdrweski@gmail.com>
2025-01-23 18:06:52 +0000

    update car engine image handling with responsive images

--- src/components/pages/PartnersPage/PartnersPage.jsx
+++ src/components/pages/PartnersPage/PartnersPage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 import Divider from "../../common/Divider";
+import ResponsiveImage from "../../common/ResponsiveImage";
 
-import carEngine from "../../../../src/assets/images/home_tuning_offer1.jpg";
+import carEngine480w from "../../../../src/assets/images/car-engine-480w.webp";
+import carEngine640w from "../../../../src/assets/images/car-engine-640w.webp";
+import carEngine768w from "../../../../src/assets/images/car-engine-768w.webp";
+import carEngine960w from "../../../../src/assets/images/car-engine-960w.webp";
+import carEngine960wFallback from "../../../../src/assets/images/car-engine-960w-fallback.jpg";
 
 import aCunningham from "../../../../src/assets/images/A-Cunningham-1024x768-1.png";
 import abWight from "../../../../src/assets/images/AB-wight-1024x768-1.png";
@@ -28,6 +33,7 @@ const PartnersPage = () => {
     { src: starvin, name: "Starvin" },
     { src: monteCassino, name: "Monte Cassino" },
   ];
+
   return (
     <section>
       <article>
@@ -51,9 +57,19 @@ const PartnersPage = () => {
             </p>
           </div>
           <Divider size="128px" orientation="vertical" />
-          <figure className="basis-1/2 pl-[2vw] pt-12">
-            <img src={carEngine} alt="Car engine" className="ml-auto" />
-          </figure>
+          <div className="basis-1/2 pl-[2vw] pt-12">
+            <ResponsiveImage
+              sources={[
+                { srcSet: carEngine480w, media: "(max-width: 480px)" },
+                { srcSet: carEngine640w, media: "(max-width: 640px)" },
+                { srcSet: carEngine768w, media: "(max-width: 768px)" },
+                { srcSet: carEngine640w, media: "(max-width: 1280px)" },
+                { srcSet: carEngine960w, media: "(min-width: 1280px)" },
+              ]}
+              fallback={carEngine960wFallback}
+              alt="Modified cars in workshop garage"
+            />
+          </div>
         </div>
         <div className="mx-auto grid grid-cols-5 gap-4 pb-10 pt-5 md:max-w-[748px] lg:max-w-[1004px] xl:max-w-[1260px]">
           {partners.map((partner, index) => (
