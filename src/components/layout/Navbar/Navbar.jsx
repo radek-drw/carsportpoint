ec35a1cdcd587d118db4f23108fb8adf2542b879
Radek Drweski <rdrweski@gmail.com>
2025-01-28 17:43:43 +0000

    simplify sticky behavior

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import NavItem from "./NavItem";
 const Navbar = () => {
   const [active, setActive] = useState("home");
   const [isSticky, setIsSticky] = useState(false);
-  const [scrollPosition, setScrollPosition] = useState(0);
   const navRef = useRef(null);
 
   const menuItems = [
@@ -22,14 +21,11 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const currentScroll = window.scrollY;
-
       if (navRef.current) {
         const navHeight = navRef.current.offsetHeight;
+        const scrollPosition = window.scrollY;
 
-        setIsSticky(currentScroll > navHeight);
-
-        setScrollPosition(Math.min(currentScroll, navHeight));
+        setIsSticky(scrollPosition > navHeight);
       }
     };
 
@@ -40,21 +36,15 @@ const Navbar = () => {
     };
   }, []);
 
-  // Dynamically calculate translateY based on scroll position
-  const translateY = isSticky ? 0 : -scrollPosition;
-
   return (
     <>
       <nav
         ref={navRef}
-        className={`flex h-24 items-center justify-between bg-white transition-all duration-1000 ${
+        className={`flex h-24 items-center justify-between ${
           isSticky
-            ? "sticky top-0 z-50 bg-[rgba(255,255,255,0.7)] shadow-md"
+            ? "sticky top-0 z-50 h-14 bg-[rgba(255,255,255,0.8)] shadow-md"
             : "relative"
         }`}
-        style={{
-          transform: `translateY(${translateY}px)`,
-        }}
         aria-label="Main navigation"
       >
         <Logo
@@ -70,7 +60,6 @@ const Navbar = () => {
               href={item.href}
               isActive={active === item.label}
               onClick={() => setActive(item.label)}
-              isSticky={isSticky}
             />
           ))}
         </ul>
