fb689c3569b8bd57eb883c300f71771be087c0c2
Radek Drweski <rdrweski@gmail.com>
2025-02-04 17:47:41 +0000

    rename file to .jsx after migrating to Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Layout from "./components/layout/Layout";
 import HomePage from "./components/pages/HomePage/HomePage";
 import AboutUsPage from "./components/pages/AboutUsPage/AboutUsPage";
