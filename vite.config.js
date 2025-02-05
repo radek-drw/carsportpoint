a60d76bb13ee406873fa80a28fd06388e759e6fe
Radek Drweski <rdrweski@gmail.com>
2025-02-05 05:39:15 +0000

    enhance Vite configuration with path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/carsportpoint/",
   server: {
+    port: 3000,
     open: true,
+  },
+  resolve: {
+    alias: {
+      "@images": path.resolve(__dirname, "src/assets/images"),
+      "@common": path.resolve(__dirname, "src/components/common"),
+    },
   },
 });
