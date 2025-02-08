4437244a41a0804c317d9ca2bb36b1463fe0165a
Radek Drweski <rdrweski@gmail.com>
2025-02-08 02:25:41 +0000

    refactor component to implement mobile menu functionality

--- src/components/layout/Navbar/NavItem.jsx
+++ src/components/layout/Navbar/NavItem.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavItem = ({ label, href, isActive }) => {
-  const baseClasses = `flex items-center justify-center h-full px-4 font-navigation uppercase text-navDesktop transition-all duration-300`;
+const NavItem = ({ label, href, isActive, onClick, isMobile }) => {
+  // Base classes
+  const mobileBaseClasses = "flex flex-col items-start text-xs px-4";
+  const desktopBaseClasses =
+    "flex items-center justify-center h-full px-4 text-base text-navDesktop transition-all duration-300";
+
+  // Dynamic classes
   const activeClasses =
     "shadow-[inset_0_-4px_0_0] shadow-customRed text-customRed";
   const hoverClasses =
     "hover:shadow-[inset_0_-4px_0_0] hover:shadow-customRed hover:text-customRed";
 
+  // Classes for <Link>
+  const getLinkClasses = () => {
+    if (isMobile) {
+      return `${mobileBaseClasses} ${isActive ? "text-white" : "text-[#A6A6A6]"}`;
+    } else {
+      return `${desktopBaseClasses} ${isActive ? activeClasses : hoverClasses}`;
+    }
+  };
+
+  // Classes for <li>
+  const getContainerClasses = () => {
+    return isMobile
+      ? "border-t-[1px] border-solid border-[#222] py-3 last:border-b-[1px]"
+      : "h-full";
+  };
+
   return (
-    <li className="h-full">
+    <li className={getContainerClasses()}>
       <Link
         to={href}
-        className={`${baseClasses} ${isActive ? activeClasses : hoverClasses}`}
+        className={getLinkClasses()}
         aria-current={isActive ? "page" : undefined}
+        onClick={onClick}
       >
         {label}
       </Link>
--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -6,7 +6,9 @@ import NavItem from "./NavItem";
 const Navbar = () => {
   const location = useLocation();
   const [isSticky, setIsSticky] = useState(false);
-  const navRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navbarRef = useRef(null);
+  const mobileMenuRef = useRef(null);
 
   const menuItems = [
     { label: "home", href: "/" },
@@ -18,10 +20,9 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (navRef.current) {
-        const navHeight = navRef.current.offsetHeight;
+      if (navbarRef.current) {
+        const navHeight = navbarRef.current.offsetHeight;
         const scrollPosition = window.scrollY;
-
         setIsSticky(scrollPosition > navHeight);
       }
     };
@@ -33,31 +34,104 @@ const Navbar = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    const handleClickOutside = (event) => {
+      if (
+        mobileMenuRef.current &&
+        !mobileMenuRef.current.contains(event.target)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
   return (
-    <nav
-      ref={navRef}
-      className={`flex items-center justify-between ${
-        isSticky
-          ? "sticky top-0 z-50 h-14 bg-[rgba(255,255,255,0.8)] shadow-md"
-          : "relative h-24"
-      }`}
-      aria-label="Main navigation"
-    >
-      <Logo
-        isClickable="true"
-        className={`ml-5 ${isSticky ? "max-w-[90px]" : "max-w-[225px]"}`}
-      />
-      <ul className="flex h-full">
-        {menuItems.map((item, index) => (
-          <NavItem
-            key={index}
-            label={item.label}
-            href={item.href}
-            isActive={location.pathname === item.href}
+    <>
+      {/* Overlay z półprzezroczystym tłem */}
+      <div
+        className={`fixed inset-0 z-40 bg-black transition-opacity duration-300 ${
+          isMenuOpen ? "opacity-60" : "pointer-events-none opacity-0"
+        }`}
+        onClick={() => setIsMenuOpen(false)}
+      ></div>
+
+      <nav
+        ref={navbarRef}
+        className={`flex items-center justify-end font-navigation uppercase md:justify-between ${
+          isSticky
+            ? "md:sticky md:top-0 md:z-50 md:h-14 md:bg-[rgba(255,255,255,0.8)] md:shadow-md"
+            : "relative h-24"
+        }`}
+        aria-label="Main navigation"
+      >
+        <div className="absolute left-1/2 -translate-x-1/2 transform">
+          <Logo
+            isClickable={true}
+            className={`${isSticky ? "md:max-w-[90px]" : "max-w-[225px]"}`}
           />
-        ))}
-      </ul>
-    </nav>
+        </div>
+
+        {/* Ikona burgera (widoczna tylko na mobile) */}
+        <button
+          className="z-50 p-5 focus:outline-none md:hidden md:p-0"
+          onClick={() => setIsMenuOpen(true)}
+          aria-label="Open menu"
+        >
+          <span className="text-3xl text-customRed">&#9776;</span>
+        </button>
+
+        {/* Menu dla desktop */}
+        <ul className="hidden h-full md:flex">
+          {menuItems.map((item, index) => (
+            <NavItem
+              key={index}
+              label={item.label}
+              href={item.href}
+              isActive={location.pathname === item.href}
+              isMobile={false}
+            />
+          ))}
+        </ul>
+
+        {/* Menu dla mobile */}
+        <div
+          ref={mobileMenuRef}
+          className={`fixed right-0 top-0 z-50 h-full w-64 transform bg-[#191919] shadow-lg transition-transform duration-300 md:hidden ${
+            isMenuOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <button
+            className="text-burgerItemColor absolute right-5 top-1 text-4xl"
+            onClick={() => setIsMenuOpen(false)}
+            aria-label="Close menu"
+          >
+            &times;
+          </button>
+          <ul className="flex h-full flex-col justify-items-start pt-32">
+            {menuItems.map((item, index) => (
+              <NavItem
+                key={index}
+                label={item.label}
+                href={item.href}
+                isActive={location.pathname === item.href}
+                onClick={() => setIsMenuOpen(false)}
+                isMobile={true}
+              />
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
