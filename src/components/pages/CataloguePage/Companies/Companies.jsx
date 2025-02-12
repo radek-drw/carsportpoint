b2ce56c0d01874d6e5a3db69f6cab7e6b6598b73
Radek Drweski <rdrweski@gmail.com>
2025-02-12 06:00:00 +0000

    refactor component to manage state and integrate CompanyList and ResetMapButton

--- src/components/pages/CataloguePage/Companies/Companies.jsx
+++ src/components/pages/CataloguePage/Companies/Companies.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import companyData from "./companyData.json";
+import ResetMapButton from "./ResetMapButton";
+import CompanyList from "./CompanyList";
 import CompanyMap from "./CompanyMap";
 
-const App = () => {
+const Companies = () => {
+  const [hoveredCompany, setHoveredCompany] = useState(null);
+  const [activeCompany, setActiveCompany] = useState(null);
+
+  const handleResetMap = () => {
+    setActiveCompany(null);
+  };
+
   return (
-    <div>
-      <CompanyMap />
-    </div>
+    <article className="flex flex-col">
+      <div className="space-y-4">
+        <CompanyList
+          companyData={companyData}
+          setHoveredCompany={setHoveredCompany}
+          setActiveCompany={setActiveCompany}
+        />
+
+        <ResetMapButton onReset={handleResetMap} />
+      </div>
+      <CompanyMap
+        companyData={companyData}
+        hoveredCompany={hoveredCompany}
+        activeCompany={activeCompany}
+      />
+    </article>
   );
 };
 
-export default App;
+export default Companies;
