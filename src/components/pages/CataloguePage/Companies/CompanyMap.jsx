51b705ff4225c6b45b4970ee5faa2c8670e9d6db
Radek Drweski <rdrweski@gmail.com>
2025-02-12 06:01:05 +0000

    enhance component to dynamically center and zoom based on active company, and update marker styles based on hover and active states

--- src/components/pages/CataloguePage/Companies/CompanyMap.jsx
+++ src/components/pages/CataloguePage/Companies/CompanyMap.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 
 const mapContainerStyle = {
@@ -6,40 +6,63 @@ const mapContainerStyle = {
   height: "600px",
 };
 
-const defaultCenter = {
+const center = {
   lat: 53.41291,
   lng: -8.24389,
 };
 
-const markerPosition = { lat: 53.44427112616309, lng: -6.209359075248016 };
-
 const defaultZoom = 7;
 
-const CompanyMap = () => {
+const CompanyMap = ({ companyData, hoveredCompany, activeCompany }) => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
-  if (!isLoaded) return <div>Loading Maps...</div>;
+  const [map, setMap] = useState(null);
 
-  const onLoad = (marker) => {
-    console.log("marker: ", marker);
-  };
+  useEffect(() => {
+    if (map) {
+      if (activeCompany) {
+        map.panTo({ lat: activeCompany.lat, lng: activeCompany.lng });
+        map.setZoom(12);
+      } else {
+        map.panTo(center);
+        map.setZoom(defaultZoom);
+      }
+    }
+  }, [activeCompany, map]);
+
+  if (!isLoaded) return <div>Loading Maps...</div>;
 
   return (
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
       zoom={defaultZoom}
-      center={defaultCenter}
+      center={
+        activeCompany
+          ? { lat: activeCompany.lat, lng: activeCompany.lng }
+          : center
+      }
+      onLoad={(mapInstance) => setMap(mapInstance)}
     >
-      <MarkerF
-        position={markerPosition}
-        onLoad={onLoad}
-        icon={{
-          url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
-          scaledSize: new window.google.maps.Size(40, 40),
-        }}
-      />
+      {companyData.map((company) => (
+        <MarkerF
+          key={company.id}
+          position={{ lat: company.lat, lng: company.lng }}
+          icon={{
+            url:
+              hoveredCompany?.id === company.id ||
+              activeCompany?.id === company.id
+                ? "https://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                : "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+            scaledSize:
+              hoveredCompany?.id === company.id ||
+              activeCompany?.id === company.id
+                ? new window.google.maps.Size(50, 50)
+                : new window.google.maps.Size(30, 30),
+          }}
+        />
+      ))}
     </GoogleMap>
   );
 };
