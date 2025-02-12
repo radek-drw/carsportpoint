df495726c082e3d54f3da43d7e9096aa923fbde2
Radek Drweski <rdrweski@gmail.com>
2025-02-12 17:06:16 +0000

    refactor button for improved styling and consistency

--- src/components/pages/CataloguePage/Companies/ResetMapButton.jsx
+++ src/components/pages/CataloguePage/Companies/ResetMapButton.jsx
@@ -1,9 +1,6 @@
 const ResetMapButton = ({ onReset }) => {
   return (
-    <button
-      className="mt-4 rounded bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
-      onClick={onReset}
-    >
+    <button className="blue-btn btn transition" onClick={onReset}>
       Reset Map View
     </button>
   );
