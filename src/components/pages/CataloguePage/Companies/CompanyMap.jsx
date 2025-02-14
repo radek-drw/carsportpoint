a63e3292afc21a7ba8ed0932065c32af591bc0ef
Radek Drweski <rdrweski@gmail.com>
2025-02-14 08:36:38 +0000

    refactor component to use local companyData and improve layout structure

--- src/components/pages/CataloguePage/Companies/CompanyMap.jsx
+++ src/components/pages/CataloguePage/Companies/CompanyMap.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 
 import CompanyInfoWindow from "./CompanyInfoWindow";
+import companyData from "./companyData.json";
 
 import { useCompany } from "@context/CompanyContext";
 
@@ -18,7 +19,7 @@ const center = {
 
 const defaultZoom = 7;
 
-const CompanyMap = ({ companyData }) => {
+const CompanyMap = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
@@ -43,38 +44,40 @@ const CompanyMap = ({ companyData }) => {
   if (!isLoaded) return <div>Loading Maps...</div>;
 
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      zoom={defaultZoom}
-      center={
-        activeCompany
-          ? { lat: activeCompany.lat, lng: activeCompany.lng }
-          : center
-      }
-      onLoad={(mapInstance) => setMap(mapInstance)}
-    >
-      {companyData.map((company) => (
-        <MarkerF
-          key={company.id}
-          position={{ lat: company.lat, lng: company.lng }}
-          onClick={() => setSelectedCompany(company)}
-          icon={{
-            url:
-              hoveredCompany?.id === company.id ||
-              activeCompany?.id === company.id
-                ? "https://maps.google.com/mapfiles/ms/icons/red-dot.png"
-                : "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-            scaledSize:
-              hoveredCompany?.id === company.id ||
-              activeCompany?.id === company.id
-                ? new window.google.maps.Size(50, 50)
-                : new window.google.maps.Size(30, 30),
-          }}
-        />
-      ))}
+    <div className="flex-grow">
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={defaultZoom}
+        center={
+          activeCompany
+            ? { lat: activeCompany.lat, lng: activeCompany.lng }
+            : center
+        }
+        onLoad={(mapInstance) => setMap(mapInstance)}
+      >
+        {companyData.map((company) => (
+          <MarkerF
+            key={company.id}
+            position={{ lat: company.lat, lng: company.lng }}
+            onClick={() => setSelectedCompany(company)}
+            icon={{
+              url:
+                hoveredCompany?.id === company.id ||
+                activeCompany?.id === company.id
+                  ? "https://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                  : "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+              scaledSize:
+                hoveredCompany?.id === company.id ||
+                activeCompany?.id === company.id
+                  ? new window.google.maps.Size(50, 50)
+                  : new window.google.maps.Size(30, 30),
+            }}
+          />
+        ))}
 
-      <CompanyInfoWindow />
-    </GoogleMap>
+        <CompanyInfoWindow />
+      </GoogleMap>
+    </div>
   );
 };
 
