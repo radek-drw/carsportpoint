ff38a99ef20d6a82792e3004a68eccd372df88e0
Radek Drweski <rdrweski@gmail.com>
2025-02-19 05:55:35 +0000

    rename component to MapContainer

--- src/components/pages/CataloguePage/Companies/Companies.jsx
+++ src/components/pages/CataloguePage/Companies/Companies.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import { CompanyProvider } from "@context/CompanyContext";
 
 import CompanyList from "./CompanyList";
-import CompanyMap from "./CompanyMap";
+import MapContainer from "./MapContainer";
 
 const Companies = () => {
   return (
     <CompanyProvider>
       <article className="mx-auto mb-24 flex max-w-[1280px] flex-col md:px-10 lg:flex-row">
         <CompanyList />
-        <CompanyMap />
+        <MapContainer />
       </article>
     </CompanyProvider>
   );
