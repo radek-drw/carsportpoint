b6e9e648b6adf16d7cb48bbab8f2d651bac8ce2a
Radek Drweski <rdrweski@gmail.com>
2025-02-20 13:33:47 +0000

    refactor: rename setActiveCompany to setSelectedCompany

--- src/components/pages/CataloguePage/Companies/ResetMapButton.jsx
+++ src/components/pages/CataloguePage/Companies/ResetMapButton.jsx
@@ -1,11 +1,11 @@
 import { useCompany } from "@context/CompanyContext";
 
 const ResetMapButton = () => {
-  const { setActiveCompany } = useCompany();
+  const { setSelectedCompany } = useCompany();
 
   return (
     <div className="mb-10 mt-4">
-      <button className="btn blue-btn" onClick={() => setActiveCompany(null)}>
+      <button className="btn blue-btn" onClick={() => setSelectedCompany(null)}>
         Reset Map View
       </button>
     </div>
