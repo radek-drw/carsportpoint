796b1c2cbe0efc61d4f59316a2991487991b1627
Radek Drweski <rdrweski@gmail.com>
2025-02-26 05:22:42 +0000

    feat: add formData and imagePreviews state

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -6,12 +6,24 @@ export const useAdmin = () => useContext(AdminContext);
 
 export const AdminProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    opening_hours: "",
+    images: [],
+  });
+  const [imagePreviews, setImagePreviews] = useState([]);
 
   return (
     <AdminContext.Provider
       value={{
         isLoggedIn,
         setIsLoggedIn,
+        formData,
+        setFormData,
+        imagePreviews,
+        setImagePreviews,
       }}
     >
       {children}
