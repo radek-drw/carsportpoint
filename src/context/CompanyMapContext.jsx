85d3ff1648d7b53d165253df6a3d51d41779c0d9
Radek Drweski <rdrweski@gmail.com>
2025-03-02 17:06:36 +0000

    refactor: replace local data with API data fetching (from AWS dynamoDB)

--- src/context/CompanyMapContext.jsx
+++ src/context/CompanyMapContext.jsx
@@ -1,18 +1,37 @@
-import { createContext, useContext, useState, useRef } from "react";
+import { createContext, useContext, useState, useRef, useEffect } from "react";
+
+import axios from "axios";
 
 const CompanyMapContext = createContext();
 
 export const useCompany = () => useContext(CompanyMapContext);
 
+const API_URL =
+  "https://cr6hha4le3.execute-api.eu-west-1.amazonaws.com/getCompanies";
+
 export const CompanyProvider = ({ children }) => {
   const [map, setMap] = useState(null);
   const [selectedCompany, setSelectedCompany] = useState(null);
   const [hoveredCompany, setHoveredCompany] = useState(null);
-  const [isGalleryOpen, setIsGalleryOpen] = useState(null);
+  const [isGalleryOpen, setIsGalleryOpen] = useState(false);
   const [defaultZoom, setDefaultZoom] = useState(null);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [galleryImages, setGalleryImages] = useState([]);
+  const [companies, setCompanies] = useState([]);
   const mapRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setCompanies(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <CompanyMapContext.Provider
@@ -31,6 +50,8 @@ export const CompanyProvider = ({ children }) => {
         setCurrentImageIndex,
         galleryImages,
         setGalleryImages,
+        companies,
+        setCompanies,
         mapRef,
       }}
     >
