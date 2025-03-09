c0b807eadf33ea6bc99916598730b5fa40591702
Radek Drweski <rdrweski@gmail.com>
2025-03-09 08:59:27 +0000

    feat: implement loading skeleton for companies list while fetching data

--- src/components/pages/CataloguePage/Companies/CompanyList.jsx
+++ src/components/pages/CataloguePage/Companies/CompanyList.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ResetMapButton from "./ResetMapButton";
 import { useCompany } from "@context/CompanyMapContext";
 
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const CompanyList = () => {
   const {
     companies,
+    isLoading,
     setSelectedCompany,
     hoveredCompany,
     setHoveredCompany,
@@ -21,22 +25,29 @@ const CompanyList = () => {
   return (
     <div className="basis-1/2 md:pr-[1vw]">
       <ul className="rounded-lg shadow-lg">
-        {companies.map((company) => (
-          <li
-            key={company.id}
-            className={`cursor-pointer px-4 py-2 transition hover:bg-gray-200 ${
-              hoveredCompany?.id === company.id
-                ? "bg-gray-200"
-                : "bg-transparent"
-            }`}
-            onMouseEnter={() => setHoveredCompany(company)}
-            onMouseLeave={() => setHoveredCompany(null)}
-            onClick={() => handleCompanyClick(company)}
-          >
-            <strong>{company.name}</strong>
-            <p>{company.address}</p>
-          </li>
-        ))}
+        {isLoading
+          ? [...Array(6)].map((_, index) => (
+              <li key={index} className="px-4 py-2">
+                <Skeleton height={22} width="80%" />
+                <Skeleton height={15} width="60%" />
+              </li>
+            ))
+          : companies.map((company) => (
+              <li
+                key={company.id}
+                className={`cursor-pointer px-4 py-2 transition hover:bg-gray-200 ${
+                  hoveredCompany?.id === company.id
+                    ? "bg-gray-200"
+                    : "bg-transparent"
+                }`}
+                onMouseEnter={() => setHoveredCompany(company)}
+                onMouseLeave={() => setHoveredCompany(null)}
+                onClick={() => handleCompanyClick(company)}
+              >
+                <strong>{company.name}</strong>
+                <p>{company.address}</p>
+              </li>
+            ))}
       </ul>
       <ResetMapButton />
     </div>
