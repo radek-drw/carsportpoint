bda3cdd99dc16dd4e4247409e2b7f354076544a4
Radek Drweski <rdrweski@gmail.com>
2025-03-14 00:02:01 +0000

    refactor: create validation schema function to allow dynamic max file size

--- src/components/common/ContactForm/validationSchema.js
+++ src/components/common/ContactForm/validationSchema.js
@@ -1,9 +1,6 @@
 import * as Yup from "yup";
 import { parsePhoneNumberFromString } from "libphonenumber-js";
 
-export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
-export const MAX_FILES = 5;
-
 /* NOTE: When adding a new file type in SUPPORTED_FORMATS, make sure to update two places:
   
   1. The 'accept' attribute in the <input> tag to allow the selection of the new file extensions. 
@@ -27,43 +24,46 @@ export const SUPPORTED_FORMATS = [
   "text/plain",
 ];
 
-export const validationSchema = Yup.object({
-  name: Yup.string().trim().max(50, "Name cannot exceed 50 characters"),
-  email: Yup.string()
-    .required("Email is required")
-    .email("Invalid email address")
-    .max(320, "Email cannot exceed 320 characters"),
-  phone: Yup.string().test(
-    "isValidPhone",
-    "Invalid phone number",
-    (value, context) => {
-      if (!value) return true;
-      const phoneNumber = parsePhoneNumberFromString(
-        value,
-        context.parent.country,
-      );
-      return phoneNumber && phoneNumber.isValid();
-    },
-  ),
-  message: Yup.string()
-    .trim()
-    .required("Message is required")
-    .max(1000, "Message cannot exceed 1000 characters"),
-  subject: Yup.string().trim().max(100, "Subject cannot exceed 100 characters"),
-  files: Yup.array()
-    .of(Yup.mixed())
-    .test(
-      "fileSize",
-      `Each file must be less than ${(MAX_FILE_SIZE / (1024 * 1024)).toFixed()}MB.`,
-      (files) =>
-        Array.isArray(files) &&
-        files.every((file) => file.size <= MAX_FILE_SIZE),
-    )
-    .test(
-      "fileType",
-      "Invalid file type.",
-      (files) =>
-        Array.isArray(files) &&
-        files.every((file) => SUPPORTED_FORMATS.includes(file.type)),
+export const createValidationSchema = (maxFileSize) =>
+  Yup.object({
+    name: Yup.string().trim().max(50, "Name cannot exceed 50 characters"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email address")
+      .max(320, "Email cannot exceed 320 characters"),
+    phone: Yup.string().test(
+      "isValidPhone",
+      "Invalid phone number",
+      (value, context) => {
+        if (!value) return true;
+        const phoneNumber = parsePhoneNumberFromString(
+          value,
+          context.parent.country,
+        );
+        return phoneNumber && phoneNumber.isValid();
+      },
     ),
-});
+    message: Yup.string()
+      .trim()
+      .required("Message is required")
+      .max(1000, "Message cannot exceed 1000 characters"),
+    subject: Yup.string()
+      .trim()
+      .max(100, "Subject cannot exceed 100 characters"),
+    files: Yup.array()
+      .of(Yup.mixed())
+      .test(
+        "fileSize",
+        `Each file must be less than ${(maxFileSize / (1024 * 1024)).toFixed()}MB.`,
+        (files) =>
+          Array.isArray(files) &&
+          files.every((file) => file.size <= maxFileSize),
+      )
+      .test(
+        "fileType",
+        "Invalid file type.",
+        (files) =>
+          Array.isArray(files) &&
+          files.every((file) => SUPPORTED_FORMATS.includes(file.type)),
+      ),
+  });
