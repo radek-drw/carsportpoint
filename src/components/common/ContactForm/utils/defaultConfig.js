c7d5dda31803ba2a28dfef243fe24498a51a4fd7
Radek Drweski <rdrweski@gmail.com>
2025-03-31 18:26:29 +0100

    feat: add default configuration for contact form fields

---
+++ src/components/common/ContactForm/utils/defaultConfig.js
@@ -0,0 +1,8 @@
+export const defaultConfig = {
+  name: { label: "Name", placeholder: "Enter your name" },
+  email: { label: "Email", placeholder: "Enter your email" },
+  phone: { label: "Phone", placeholder: "Enter your phone number" },
+  subject: { label: "Subject", placeholder: "Enter your subject" },
+  message: { label: "Message", placeholder: "Your message here", rows: 6 },
+  files: { label: "Upload Files", maxFilesCount: 5, maxFileSize: 5 },
+};
