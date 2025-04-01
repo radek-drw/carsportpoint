9cf4f9e5e48e92a2c7995c915dc2e05e1dd07895
Radek Drweski <rdrweski@gmail.com>
2025-04-01 05:40:09 +0100

    feat: add default country for phone field

--- src/components/common/ContactForm/utils/defaultConfig.js
+++ src/components/common/ContactForm/utils/defaultConfig.js
@@ -1,7 +1,11 @@
 export const defaultConfig = {
   name: { label: "Name", placeholder: "Enter your name" },
   email: { label: "Email", placeholder: "Enter your email" },
-  phone: { label: "Phone", placeholder: "Enter your phone number" },
+  phone: {
+    label: "Phone",
+    placeholder: "Enter your phone number",
+    country: "IE",
+  },
   subject: { label: "Subject", placeholder: "Enter your subject" },
   message: { label: "Message", placeholder: "Your message here", rows: 6 },
   files: { label: "Upload Files", maxFilesCount: 5, maxFileSize: 5 },
