bfa3226237777b2abeffdf8d528c22b77a623f0c
Radek Drweski <rdrweski@gmail.com>
2025-04-02 16:47:22 +0100

    feat: add required fields to ContactForm default configuration

--- src/components/common/ContactForm/utils/defaultConfig.js
+++ src/components/common/ContactForm/utils/defaultConfig.js
@@ -1,13 +1,36 @@
 export const defaultConfig = {
-  name: { label: "Name", placeholder: "Enter your name" },
-  email: { label: "Email", placeholder: "Enter your email" },
+  name: {
+    label: "Name",
+    placeholder: "Enter your name",
+    required: true,
+  },
+  email: {
+    label: "Email",
+    placeholder: "Enter your email",
+    required: true,
+  },
   phone: {
     label: "Phone",
     placeholder: "Enter your phone number",
     country: "IE",
+    required: true,
   },
-  subject: { label: "Subject", placeholder: "Enter your subject" },
-  message: { label: "Message", placeholder: "Your message here", rows: 6 },
-  files: { label: "Upload Files", maxFilesCount: 5, maxFileSize: 5 },
+  subject: {
+    label: "Subject",
+    placeholder: "Enter your subject",
+    required: true,
+  },
+  message: {
+    label: "Message",
+    placeholder: "Your message here",
+    rows: 6,
+    required: true,
+  },
+  files: {
+    label: "Upload Files",
+    maxFilesCount: 5,
+    maxFileSize: 5,
+    required: false,
+  },
   buttonLabel: "Send a Message",
 };
