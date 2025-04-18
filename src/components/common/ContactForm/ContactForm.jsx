5bb4a049d5d0d8973ade7559d20395e9992bbb00
Radek Drweski <rdrweski@gmail.com>
2025-04-18 22:48:44 +0100

    refactor: extract form submission logic into handleFormSubmit utility

--- src/components/common/ContactForm/ContactForm.jsx
+++ src/components/common/ContactForm/ContactForm.jsx
@@ -14,8 +14,9 @@ import SuccessMessage from "./formSubmitStatus/SuccessMessage";
 import ErrorMessage from "./formSubmitStatus/ErrorMessage";
 
 import { defaultConfig } from "./utils/defaultConfig";
-import { sanitizeFileName } from "./utils/sanitizeFileName";
+
 import validateProps from "./utils/validateProps";
+import { handleFormSubmit } from "./utils/handleFormSubmit";
 
 import { getValidationSchema } from "./validationSchema";
 
@@ -53,65 +54,20 @@ const ContactForm = ({
   const MESSAGE_DELAY = 100;
   const MESSAGE_DURATION = 5000;
 
-  const SEND_CONTACT_FORM_URL =
-    "https://qxzl1w9qrh.execute-api.eu-west-1.amazonaws.com/sendContactForm";
-
-  const S3_UPLOAD_URL =
-    "https://76dy56vmwa.execute-api.eu-west-1.amazonaws.com/uploadFilesToS3";
-
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={getValidationSchema(customConfig)}
-      onSubmit={async (values, { setSubmitting, resetForm }) => {
-        try {
-          let fileUrls = [];
-          if (values.files && values.files.length > 0) {
-            const formData = new FormData();
-            values.files.forEach((file) => {
-              const sanitizedName = sanitizeFileName(file.name);
-              const sanitizedFile = new File([file], sanitizedName, {
-                type: file.type,
-              });
-              formData.append("files", sanitizedFile);
-            });
-
-            const fileUploadRes = await axios.post(S3_UPLOAD_URL, formData);
-
-            fileUrls = fileUploadRes.data.urls.map((url, index) => ({
-              url,
-              name: sanitizeFileName(values.files[index].name),
-              size: values.files[index].size,
-            }));
-          }
-          const payload = {
-            ...values,
-            files: fileUrls,
-          };
-          await axios.post(SEND_CONTACT_FORM_URL, payload);
-          setSuccessMessage("");
-          clearTimeout(successTimeoutRef.current);
-          setTimeout(() => {
-            setSuccessMessage("The form has been successfully submitted!");
-          }, MESSAGE_DELAY);
-          successTimeoutRef.current = setTimeout(() => {
-            setSuccessMessage("");
-          }, MESSAGE_DURATION);
-          resetForm();
-        } catch (error) {
-          console.error("Submission error:", error);
-          setErrorMessage("");
-          clearTimeout(errorTimeoutRef.current);
-          setTimeout(() => {
-            setErrorMessage("Something went wrong. Please try again later");
-          }, MESSAGE_DELAY);
-          errorTimeoutRef.current = setTimeout(() => {
-            setErrorMessage("");
-          }, MESSAGE_DURATION);
-        } finally {
-          setSubmitting(false);
-        }
-      }}
+      onSubmit={(values, helpers) =>
+        handleFormSubmit(
+          values,
+          helpers,
+          setSuccessMessage,
+          setErrorMessage,
+          successTimeoutRef,
+          errorTimeoutRef,
+        )
+      }
     >
       {({ values, setFieldValue, isSubmitting, errors, touched }) =>
         /* prettier-ignore */
