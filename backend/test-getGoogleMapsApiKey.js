08b675882164c4db6a7d55baf096995fe64abd25
Radek Drweski <rdrweski@gmail.com>
2025-04-24 18:07:01 +0100

    chore: remove function in favor of simpler usage of environment variables with Amplify

--- backend/test-getGoogleMapsApiKey.js
+++
@@ -1,9 +0,0 @@
-import { handler } from "./getGoogleMapsApiKey.mjs";
-
-handler()
-  .then((response) => {
-    console.log("Response:", JSON.parse(response.body));
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
