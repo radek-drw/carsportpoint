0b0315f01cf7e5c0bb8b433088158b140878fce9
Radek Drweski <rdrweski@gmail.com>
2025-06-10 17:10:04 +0100

    test: update displayMode prop to "both"

--- frontend/src/components/pages/CataloguePage/CataloguePage.jsx
+++ frontend/src/components/pages/CataloguePage/CataloguePage.jsx
@@ -19,7 +19,7 @@ const CataloguePage = () => {
           Are you a company owner? Add your company to our catalogue to reach
           more customers
         </p>
-        <ContactForm displayMode="placeholder" />
+        <ContactForm displayMode="both" />
       </div>
     </section>
   );
