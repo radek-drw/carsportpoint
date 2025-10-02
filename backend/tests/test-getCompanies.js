22ee81a0da5b53f253dca53e3950e1ce15ab8edd
radek-drw <rdrweski@gmail.com>
2025-10-02 14:02:12 +0100

    Ignore backend/tests

--- backend/tests/test-getCompanies.js
+++
@@ -1,9 +0,0 @@
-import { handler } from "../functions/getCompanies.mjs";
-
-handler()
-  .then((response) => {
-    console.log("Response:", JSON.parse(response.body));
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
