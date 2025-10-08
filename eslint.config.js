e58c1d7a0312dae6a3f2b31f028510bdcd87772d
radek-drw <rdrweski@gmail.com>
2025-10-08 18:08:54 +0100

    chore: update configuration define global ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,22 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import tailwind from "eslint-plugin-tailwindcss";
+import tailwindcss from "eslint-plugin-tailwindcss";
 import importPlugin from "eslint-plugin-import";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [
+export default defineConfig([
+  globalIgnores([
+    "node_modules",
+    "dist",
+    "build",
+    "coverage",
+    "out",
+    ".tmp",
+    ".cache",
+    "public",
+  ]),
+
   js.configs.recommended,
 
   {
@@ -32,7 +44,7 @@ export default [
       "react-hooks": reactHooks,
       "jsx-a11y": jsxA11y,
       import: importPlugin,
-      tailwind,
+      tailwindcss,
     },
     settings: {
       react: {
@@ -48,8 +60,10 @@ export default [
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...jsxA11y.configs.recommended.rules,
-      ...tailwind.configs.recommended.rules,
+      ...tailwindcss.configs.recommended.rules,
       "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "tailwindcss/classnames-order": "off",
       "import/order": [
         "warn",
         {
@@ -66,4 +80,4 @@ export default [
       ],
     },
   },
-];
+]);
