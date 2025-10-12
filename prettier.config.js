1e43f22c50405f3162d0719fc59a9c54155db5d9
radek-drw <rdrweski@gmail.com>
2025-10-12 15:02:31 +0100

    add short description what the plugin does

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
+  // plugin that automatically sorts Tailwind classes in JSX/HTML
   plugins: ['prettier-plugin-tailwindcss'],
   singleQuote: true,
   trailingComma: 'es5',
